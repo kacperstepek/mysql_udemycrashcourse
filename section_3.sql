7811073630c909302c6464e15025e9003aa36dd6
kapcerz <kapcerz@gmail.com>
2022-01-27 12:16:24 +0000

    some setup files and section 3 code

---
+++ section_3.sql
@@ -0,0 +1,59 @@
+# SHOW databases;
+
+# CREATE DATABASE DogApp;
+
+# SHOW DATABASES;
+
+# USE DogApp;    
+
+# DROP DATABASE DogApp;
+
+# SHOW DATABASES;
+
+# CREATE DATABASE bakery;
+# USE bakery;
+
+# CREATE TABLE pastries(
+#     name VARCHAR(50),
+#     quantity INT
+# );
+
+# DESC pastries;
+
+# DROP TABLE pastries;
+
+# SHOW TABLES;
+
+# CREATE TABLE cats (
+#     name VARCHAR(100), 
+#     age INT
+# );
+
+# INSERT INTO cats(name, age)
+#     VALUES ('Jetson', 7);
+
+# INSERT INTO cats(age, name)
+#     VALUES (12, 'Victoria');
+
+# SELECT * FROM cats;
+
+# CREATE TABLE people(
+#     first_name VARCHAR(20), 
+#     last_name VARCHAR(20), 
+#     age INT);
+
+# DESC people;
+
+# INSERT INTO people (first_name, last_name, age)
+#     VALUES('Tina', 'Belcher', 13);
+
+# INSERT INTO people (first_name, last_name, age)
+#      VALUES('Bob', 'Belcher', 42);
+
+# INSERT INTO people(first_name, last_name, age)
+#     VALUES
+#         ('Linda', 'Belcher', 45),
+#         ('Philip', 'Frond', 38),
+#         ('Calvin', 'Fischoeder', 70);
+
+# SELECT * FROM people;
